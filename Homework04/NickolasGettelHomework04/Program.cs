224f4c0aacb5e716d231409f4d720e2dd95a69f1
nickolaskg <nickolaskg@gmail.com>
2022-06-04 19:27:46 -0400

    initialized homework04 project

---
+++ Homework04/NickolasGettelHomework04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NickolasGettelHomework04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
