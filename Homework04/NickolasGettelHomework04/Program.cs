9b8ec36874100748d558081f3f328aee533e8b56
nickolaskg <nickolaskg@gmail.com>
2022-06-05 23:16:37 -0400

    Added logic - App appears to be functioning properly. Need to refine output styling

--- Homework04/NickolasGettelHomework04/Program.cs
+++ Homework04/NickolasGettelHomework04/Program.cs
@@ -5,11 +5,11 @@ namespace NickolasGettelHomework04
 {
     class Program
     {
+         private static int[] integerArray = new int[10];
         static void Main(string[] args)
         {
             // Declare variables
             int min, max, avg, sum;
-            int[] integerArray = new int[10];
 
             // Call method to prompt user for integers to fill an array interactively /w error rejection message/prompt
             PromptForIntegers();
@@ -20,34 +20,109 @@ namespace NickolasGettelHomework04
             // Display the statistics within the main()
             WriteLine($"Lowest Value: {min} - Highest Value: {max}");
             WriteLine($"Average Value: {avg} - Sum of Values: {sum}");
+            foreach(var item in integerArray)
+            {
+                WriteLine(item);
+            }
         }
 
         public static void PromptForIntegers()
         {
             // Declare temp variable
             bool isSessionActive = true;
+            string userInput;
+            int parsedInt = 0;
+            int numberOfInputs = 0;
 
             // While Loop based on isSessionActive bool
-            // Prompt user for integer
-            // Assign value to temp variable
-            // Check for sentinel value to stop the program
-            // Yes: Stop program - No: Continue - assign bool value to isSessionActive in main()
-            // Try parsing value integer
-            // If integer is within specification
-            // Add validated integer into array
-            // Else Reprompt user for valid integer
+            while (isSessionActive)
+            {
+                // If last element in the array has a value - end loop
+                if(numberOfInputs == 10)
+                {
+                    isSessionActive = false;
+                    continue;
+                }
+
+                // Prompt user for integer
+                Write("Please enter a integer between 1 and 10: ");
+                // Assign value to temp variable
+                userInput = ReadLine();
+
+                // Check for sentinel value to stop the program
+                if(userInput.ToLower() == "stop")
+                {
+                    isSessionActive = false;
+                    continue;
+                }
+
+                // Try parsing value integer
+                int.TryParse(userInput, out parsedInt);
+                // -> If integer is within specification
+                if (parsedInt > 0 && parsedInt < 11)
+                {
+                    // -> Add validated integer into array
+                    integerArray[numberOfInputs] = parsedInt;
+                    numberOfInputs++;
+                } else
+                {
+                    // -> Else Reprompt user for valid integer
+                    WriteLine("Invalid entry. Please enter an integer between 1 and 10");
+                }
+            }
         }
 
         public static void ComputeIntegers(out int min, out int max, out int avg, out int sum)
         {
+            int tempMin = 0, tempMax = 0 , tempAvg = 0, tempSum = 0;
+
             // Determine and delcare min
-            min = 0;
+            foreach (int num in integerArray)
+            {
+                if (tempMin == 0)
+                {
+                    tempMin = num;
+                }
+                else
+                {
+                    if (num < tempMin)
+                    {
+                        tempMin = num;
+                    }
+                }
+            }
             // Determine and delcare max
-            max = 0;
+            foreach (int num in integerArray)
+            {
+                if (tempMax == 0)
+                {
+                    tempMax = num;
+                }
+                else
+                {
+                    if (num > tempMax)
+                    {
+                        tempMax = num;
+                    }
+                }
+            }
+
             // Determine and delcare avg
-            avg = 0;
+            foreach (int num in integerArray)
+            {
+                tempAvg = tempAvg + num;
+            }
+            tempAvg = tempAvg / integerArray.Length;
             // Determine and delcare sum
-            sum = 0;
+            foreach (int num in integerArray)
+            {
+                tempSum = tempSum + num;
+            }
+
+            min = tempMin;
+            max = tempMax;
+            avg = tempAvg;
+            sum = tempSum;
         }
 
     }
