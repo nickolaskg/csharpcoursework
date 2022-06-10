e5426605d07bb9440941ad974bc4a2277587bc5e
nickolaskg <nickolaskg@gmail.com>
2022-06-09 23:20:57 -0400

    added functionality to overloaded operator + method

--- Homework05/NickolasGettelHomework05/Fraction.cs
+++ Homework05/NickolasGettelHomework05/Fraction.cs
@@ -6,42 +6,101 @@ namespace NickolasGettelHomework05
 {
     class Fraction
     {
+        int wholeNumber;
+        int numerator;
+        int denominator;
 
         public int WholeNumber { get; set; }
         public int Numerator { get; set; }
         public int Denominator
         {
-            get { return Denominator; }
+            get { return denominator; }
             set
             {
                 if (value > 0)
                 {
-                    Denominator = value;
+                    denominator = value;
                 }
                 else
                 {
-                    Denominator = 1;
+                    denominator = 1;
                 }
             }
         }
 
-        Fraction(int wholeNumber, int numerator, int denominator)
+        public Fraction(int wholeNumber, int numerator, int denominator)
         {
             WholeNumber = wholeNumber;
             Numerator = numerator;
             Denominator = denominator;
         }
 
-        Fraction(int numerator, int denominator)
+        public Fraction(int numerator, int denominator)
         {
             Numerator = numerator;
             Denominator = denominator;
         }
 
-        Fraction()
+        public Fraction()
         {
             WholeNumber = 0;
             Denominator = 1;
         }
+
+        public static int Reduce(int wholeNumber, int numerator, int denominator)
+        {
+            // Place holder for actual code.
+            return 0;
+        }
+
+        public static Fraction operator +(Fraction one, Fraction two)
+        {
+            int num, den, wholeNum = 0, numOne, numTwo, denOne = one.Denominator, denTwo = two.Denominator;
+
+            // Eliminate whole number (1) by adding into fraction for both fractions
+            if (one.WholeNumber > 0)
+            {
+                numOne = one.WholeNumber * one.Denominator + one.Numerator;
+            }
+            else
+            {
+                numOne = one.Numerator;
+            }
+            // Eliminate whole number (2) by adding into fraction
+            if (two.WholeNumber > 0)
+            {
+                numTwo = two.WholeNumber * two.Denominator + two.Numerator;
+            }
+            else
+            {
+                numTwo = two.Numerator;
+            }
+
+            num = numOne * denTwo + numTwo * denOne;
+            den = denOne * denTwo;
+
+            // Check and convert improper fraction to whole number / fraction
+
+            if(num > den)
+            {
+                wholeNum = num / den;
+                num = num % den;
+            }
+
+            if(wholeNum > 0)
+            {
+                return new Fraction(wholeNum, num, den);
+            }
+            else
+            {
+                return new Fraction(num, den);
+            }
+        }
+
+        public override string ToString()
+        {
+           return WholeNumber > 0 ? $"{WholeNumber} {Numerator} / {Denominator}" : $"{Numerator} / {Denominator}";
+        }
+
     }
 }
--- Homework05/NickolasGettelHomework05/Program.cs
+++ Homework05/NickolasGettelHomework05/Program.cs
@@ -2,11 +2,25 @@
 
 namespace NickolasGettelHomework05
 {
+
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args) 
+        { 
+            Fraction test = new Fraction(2, 2, 4);
+            Fraction test2 = new Fraction(1, 4, 5);
+
+            Fraction test3 = test + test2;
+            
+            string testOutput = test3.ToString();
+            Console.WriteLine(testOutput);
+
+            string asdf = Console.ReadLine();
+
         }
+
+
+
+
     }
 }
