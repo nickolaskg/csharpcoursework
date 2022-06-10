e6da5cadab2605cb95f3edd026e22a5098951fd6
nickolaskg <nickolaskg@gmail.com>
2022-06-09 22:21:50 -0400

    scaffolded homework05 - added methods. need to add logic next

---
+++ Homework05/NickolasGettelHomework05/Fraction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NickolasGettelHomework05
+{
+    class Fraction
+    {
+
+        public int WholeNumber { get; set; }
+        public int Numerator { get; set; }
+        public int Denominator
+        {
+            get { return Denominator; }
+            set
+            {
+                if (value > 0)
+                {
+                    Denominator = value;
+                }
+                else
+                {
+                    Denominator = 1;
+                }
+            }
+        }
+
+        Fraction(int wholeNumber, int numerator, int denominator)
+        {
+            WholeNumber = wholeNumber;
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        Fraction()
+        {
+            WholeNumber = 0;
+            Denominator = 1;
+        }
+    }
+}
---
+++ Homework05/NickolasGettelHomework05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NickolasGettelHomework05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
